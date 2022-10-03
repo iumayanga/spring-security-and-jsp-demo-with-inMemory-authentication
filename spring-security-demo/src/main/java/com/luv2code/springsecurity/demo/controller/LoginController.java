099df104b5f2dce35a4a5b66afe5d79c199a45a0
iumayanga <pl.isuru97@gmail.com>
2022-10-04 03:51:54 +0530

    Initial Commit

---
+++ spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		// return "plain-login";
+		return "fancy-login";
+		// return "csrf-fancy-login";
+	}
+	
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		return "access-denied";
+	}
+}
