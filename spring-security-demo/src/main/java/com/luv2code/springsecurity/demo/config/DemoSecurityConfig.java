3c857a9956b207ac9c74975ca7ea33fa6a8718d0
iumayanga <pl.isuru97@gmail.com>
2022-10-04 15:07:27 +0530

    Remove deprecated WebSecurityConfigurerAdapter

--- spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,53 +1,67 @@
 package com.luv2code.springsecurity.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+public class DemoSecurityConfig {
 
-	// To configure users(in-memory, database,...)
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
-	}
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+    	
+        UserDetails john = User.builder()
+            .username("john")
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build();
 
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "ADMIN")
+                .build();
+        
+        return new InMemoryUserDetailsManager(john, mary, susan);        
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	
+    	return http
+		.authorizeRequests(configurer ->
+			configurer
+//				.anyRequest().authenticated()
+				.antMatchers("/").hasRole("EMPLOYEE")
+				.antMatchers("/leaders/**").hasRole("MANAGER")
+				.antMatchers("/systems/**").hasRole("ADMIN"))
 		
-	
-	// To configure security of web-paths in application(login, logout,...)
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.authorizeRequests()
-			.antMatchers("/css/**")
-				.permitAll()
-			// .anyRequest().authenticated()
-			.antMatchers("/").hasRole("EMPLOYEE")
-			.antMatchers("/leaders/**").hasRole("MANAGER")
-			.antMatchers("/systems/**").hasRole("ADMIN")
-		.and()
-			.formLogin()
+		.formLogin(configurer ->
+			configurer
 				.loginPage("/showMyLoginPage")
 				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll()
-		.and()
-			.logout().permitAll()
-		.and()
-			.exceptionHandling().accessDeniedPage("/access-denied");
-	}
+				.permitAll())
 		
+		.logout(configurer -> 
+			configurer
+				.permitAll())
+		
+		.exceptionHandling(configurer ->
+			configurer
+				.accessDeniedPage("/access-denied"))
+		
+		.build();        
+    }	
 }
-
-// In antMatchers; **=all sub-directories
